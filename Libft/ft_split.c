d6f71dd7a534591d80988ccc6c9c15cc3ec21651
edfolk <96429557+edfolk@users.noreply.github.com>
2022-04-22 10:24:44 +0200

    Add files via upload

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,15 +6,31 @@
 /*   By: ljustici <ljustici@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 08:42:18 by ljustici          #+#    #+#             */
-/*   Updated: 2022/04/22 10:00:58 by ljustici         ###   ########.fr       */
+/*   Updated: 2022/04/22 10:23:26 by ljustici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char **ft_split(char const *s, char c)
 {
-	i = 0;
-	while(s[i] != c)
-		i++;
-	ft_split[n] = (char*)malloc(i);
+	char **result;
+	size_t j;
+	size_t i;
+	size_t n;
 	
+	j = 0;
+	n = 0;
+	while (s[j])
+	{
+		i = 0;
+		while(s[i] != c)
+			i++;
+		if (i > 0)
+		{
+			result[n] = ft_substr(s, j, i);
+			n++;
+			j += i;
+		}
+		j++;
+	}
+	return (result;)
 }
