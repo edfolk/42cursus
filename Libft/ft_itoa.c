d8297b3f8a73b56e450b5fb3bdfa5ba522597edc
edfolk <96429557+edfolk@users.noreply.github.com>
2022-04-22 12:14:33 +0200

    Add files via upload

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,9 +6,32 @@
 /*   By: ljustici <ljustici@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 10:25:55 by ljustici          #+#    #+#             */
-/*   Updated: 2022/04/22 10:45:37 by ljustici         ###   ########.fr       */
+/*   Updated: 2022/04/22 12:12:12 by ljustici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	ft_nbr_to_str(int n, char *dst, int size)
+{
+	if (n == -2147483648)
+	{
+		*dest = "-2147483648";
+	}
+	else
+	{
+		if (n < 0)
+		{
+			dst[0]='-';
+			n *= -1;
+		}
+		while (size > 0)
+		{
+			if (n >= 10)
+				ft_nbr_to_str(n / 10);
+			dst[size] =(n % 10) + '0';
+			size--;
+		}
+	}
+}
 
 char *ft_itoa(int n)
 {
@@ -16,16 +39,20 @@ char *ft_itoa(int n)
 	int i;
 	int size;
 
+	if (n == -2147483648)
+	{
+		str_value = (char*)malloc(11);
+		if (!str_value)
+			return (0);
+		ft_nbr_to_str(n, str_value,11);
+		return (str_value);
+	}
 	i = 0;
 	while (n%10 != 0)
 		size++;
 	str_value = (char*)malloc(size);
 	if (!str_value)
 		return (0);
-	i = 0;
-	while (i < size/2)
-	{
-		
-		i++;
-	}
+	ft_nbr_to_str(n,str_value,size);
+	return (str_value);
 }
